9781a2b55c30232bb0d368c27115ca570bef8fbe
DESKTOP-RFD5IIO\宮岡 <bzg3804@gmail.com>
2026-06-22 11:52:58 +0900

    攻撃追加

--- Assets/Ken Movement.cs
+++ Assets/Ken Movement.cs
@@ -6,7 +6,14 @@ public class KenMovement : MonoBehaviour
     public float jumpPower = 7f; //ジャンプ力
 
     private Rigidbody2D rb;
-    private bool isGrounded = true; //キャラがステージ上にいるか判定
+    private bool isGrounded = true; //キャラがステージ上にいるか判定するフラグ
+
+    public float attackTime = 0.2f; 
+    private bool isAttacking = false; //攻撃判定のフラグ
+
+    public float specialTime  = 0.5f;
+
+
 //------------------------------------------------------------------------------
 
     void Start() //ゲーム開始時に1回だけ実行
@@ -16,25 +23,60 @@ public class KenMovement : MonoBehaviour
 
     void Update() //毎フレーム実行
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A)) //「A」キーで左に移動
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D)) //「D」キーで右に移動
         {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) //「SPACE」キーでジャンプ
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
             isGrounded = false;
         }
+
+        if (Input.GetKeyDown(KeyCode.J) && !isAttacking) //「J」キーで攻撃
+        {
+            StartCoroutine(NormalAttack());
+        }
+
+        if (Input.GetKeyDown(KeyCode.K) && !isAttacking)
+        {
+            StartCoroutine(SpecialAttack());
+        }
     }
 
+
+    //衝突判定
     void OnCollisionEnter2D(Collision2D collision)
     {
         isGrounded = true;
+    }
+
+
+    //通常攻撃の関数
+    System.Collections.IEnumerator NormalAttack()
+    {
+        isAttacking = true; 
+        Debug.Log("通常技"); //メッセージを表示
+
+        yield return new WaitForSeconds(attackTime); //攻撃硬直
+
+        isAttacking = false; 
+    }
+
+    //必殺技の関数
+    System.Collections.IEnumerator SpecialAttack()
+    {
+        isAttacking = true;
+        Debug.Log("必殺技"); //メッセージ表示
+
+        yield return new WaitForSeconds(specialTime); //攻撃硬直
+
+        isAttacking = false;
     }
 }
